dba146a8f40cbdcd51ec2a953b7ab36d80c8b9bf
hjung <helmut.jung@ard-stern.de>
2023-03-28 15:29:58 +0200

    Projektdateien hinzufügen.

---
+++ EmpowerTest_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerTest_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerTest_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C50A2AF2-142B-469D-B255-691BA0D02AEF")]
+[assembly: AssemblyVersion("1.0.0.0")]
